1610c03e65c5147c54a1da688e4ac34ae5183771
Alex <awalte11@students.towson.edu>
2019-04-28 13:02:14 -0400

    Collections now look at images, images now have details/no-details mode

--- Client/src/app/image/image.component.ts
+++ Client/src/app/image/image.component.ts
@@ -11,17 +11,23 @@ import { DataService } from '../data.service';
 export class ImageComponent implements OnInit {
 
   @Input() picture : picture;
+  @Input() details = true;
   constructor( private route : ActivatedRoute, private router : Router , private dataService: DataService) { }
 
   ngOnInit() {
-    this.getPicture();
+      this.getPicture();
+      
+    
   }
 
   getPicture() : void {
-    const id = parseInt(this.route.snapshot.paramMap.get('id'), 10);
-    if (id) {
-        this.picture = this.dataService.getPicture(id);
-        
+    if (this.details)
+    {
+      const id = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+      if (id) {
+          this.picture = this.dataService.getPicture(id);
+          this.details = true;
+      }
     }
   }
 
